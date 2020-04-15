f8bc590a9a5cae700e8ce7d21d75904d5bd28017
Paul Monnery <paul.monnery@epitech.eu>
2020-04-15 15:58:09 +0200

    [ADD] final version of go workshop

---
+++ software/2.Go/src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"poc-workshop-go/database"
+	"poc-workshop-go/routes"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+type core struct {
+	port   string
+	router *mux.Router
+}
+
+func (c *core) init() {
+	c.port = "8080"
+	c.router = mux.NewRouter()
+	routes.Init(c.router)
+}
+
+func (c *core) initDatabases() {
+	if database.Init() != nil {
+		log.Fatal("db failed")
+	}
+}
+
+func (c *core) destroy() {
+	database.Destroy()
+}
+
+func main() {
+	server := new(core)
+	server.initDatabases()
+	server.init()
+
+	defer server.destroy()
+	println("SERVER RUN ON :", server.port)
+	log.Fatal(
+		http.ListenAndServe(":"+server.port, handlers.CORS(
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"*"}))(server.router)))
+}
