e06189e9874741d8380b96a0780b3044fb476ca2
loicttn <loic.titren@epitech.eu>
2020-04-30 12:17:28 +0200

    fix(angr): avoid

--- security/angr/2-avoid/solve.py
+++ security/angr/2-avoid/solve.py
@@ -5,8 +5,8 @@ def main():
     project = angr.Project("./2-avoid/chall")
     initial_state = project.factory.entry_state()
     simulation = project.factory.simgr(initial_state)
-    good_addr = 0x00000000004012ea
-    bad_addr = [0x00000000004012a9]
+    good_addr = 0x080485e5
+    bad_addr = [0x080485a8]
     simulation.explore(find=good_addr, avoid=bad_addr)
     if simulation.found:
         solution_state = simulation.found[0]
