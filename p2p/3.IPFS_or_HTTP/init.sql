d37dec3b62472d84ccc3c870e281479a8cdde635
naadi <adina.cazalens@dataimpact.io>
2021-08-07 12:41:54 +0200

    Mod: zip everything for storage purposes

--- p2p/3.IPFS_or_HTTP/init.sql
+++
@@ -1,4 +0,0 @@
-create user postgres;
-create database postgres;
-grant all privileges on database postgres to postgres;
-create table musicfiles(id int);
--- p2p/3.IPFS_or_HTTP/launch.sh
+++
@@ -1,3 +0,0 @@
-./manage.py makemigrations
-./manage.py migrate
-./manage.py runserver 0.0.0.0:8000
