660d913b5b3b7a56651c779a14d79da8d3c89928
Adrien FORT <adrien.fort01@gmail.com>
2021-08-22 17:39:01 +0200

    feat: add temprary correction

--- software/20.Serverless/Serverless-API/setup.ts
+++ software/20.Serverless/Serverless-API/setup.ts
@@ -1,14 +1,14 @@
 import { Context, APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
-export async function hello(event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> {
+const status = (event: APIGatewayEvent, context: Context): APIGatewayProxyResult => {
 	console.log('---- EVENT ----');
 	console.log(event);
 	console.log('---- CONTEXT ----');
 	console.log(context);
 	return {
 		statusCode: 200,
-		body: JSON.stringify({
-			message: 'It is working !',
-		}),
+		body: JSON.stringify({ message: 'It is working !' }),
 	};
-}
+};
+
+export { status };
--- software/20.Serverless/Serverless-API/tests/setup.test.ts
+++ software/20.Serverless/Serverless-API/tests/setup.test.ts
@@ -1,12 +1,13 @@
 import { APIGatewayEvent, Context } from 'aws-lambda';
-import * as handler from '../handler';
+import * as setup from '../setup';
 
-test('hello', async () => {
+test('status', async () => {
 	const event = { body: 'Test Body' } as APIGatewayEvent;
 	const context = {} as Context;
 
-	const response = await handler.hello(event, context);
+	const response = await setup.status(event, context);
 
 	expect(response.statusCode).toEqual(200);
 	expect(typeof response.body).toBe('string');
+	expect(response.body).toEqual('{"message":"It is working !"}');
 });
