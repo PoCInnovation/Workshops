6031495ecf17c71ff83e3e07c79a32fbedb59525
Lucien <lucien.carneiro@protonmail.com>
2022-05-31 14:44:34 +0200

    add: workshop html - hidden

---
+++ security/web/html/hidden/static/script.js
@@ -0,0 +1,17 @@
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
