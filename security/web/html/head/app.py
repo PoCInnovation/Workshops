9543d8cc3dca11ffe362ead0af8a35a844c8d410
Lucien <lucien.carneiro@protonmail.com>
2022-06-15 13:59:17 +0200

    :add workshop html - head challenge

--- security/web/html/head/app.py
+++ security/web/html/head/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, request
+from flask import Flask, render_template, make_response, request
 import os
 
 
@@ -7,7 +7,17 @@ app = Flask(__name__, template_folder='template', static_folder='static')
 
 @app.route('/')
 def home():
+    if (request.headers.get('User-aagent') == 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'):
+        if (request.headers.get('PoC-HTML-2022') == 'Can I get the flag, please?'):
+            return render_template('flag.html', flag=os.getenv('FLAG'))
     return render_template('index.html')
+
+
+@app.route('/robots.txt', methods=['GET'])
+def robots():
+  response = make_response(open('static/robots.txt').read())
+  response.headers["Content-type"] = "text/plain"
+  return response
 
 
 if __name__ == "__main__":
