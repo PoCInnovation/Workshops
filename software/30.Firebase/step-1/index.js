520a59ad8c114f6f6c34ca34c6450bfac3e81275
Toni Da rodda <toni.da-rodda@epitech.eu>
2022-09-03 18:20:29 +0200

    Update: repo and add task

---
+++ software/30.Firebase/step-1/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore
+} from 'firebase/firestore'
+import { getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA2e7Qg9ZamfWhZ6QyQkTwEoMWdsdmJLNc",
+  authDomain: "try-something-new-206a1.firebaseapp.com",
+  projectId: "try-something-new-206a1",
+  storageBucket: "try-something-new-206a1.appspot.com",
+  messagingSenderId: "613338364088",
+  appId: "1:613338364088:web:f174b955e4ea3511c6dc4d",
+  measurementId: "G-XDKE66K47L"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+const auth = getAuth();
+const app = express();
+
+app.use(express.json());
+
+async function register(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    await createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      res.send(cred.user);
+    }).catch((err) => {
+      res.send(err.message);
+    });
+}
+
+app.post('/register', register);
+
+app.listen(4000, () =>
+  console.log(`Example app listen on port: ${4000}!`),
+);
